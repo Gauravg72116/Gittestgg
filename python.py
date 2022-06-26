fcf83ec53e5be3e479f1b5cd98740bac1d1855db
gauravg <gauravg@gmail.com>
2022-06-26 20:19:00 +0530

    latest code

--- python.py
+++ python.py
@@ -1,2 +1,19 @@
 print hellofolk
  print copy jboss prod to ndc utility and app 3 & 4
+ rename jboss folder in app 3 & 4
+copy jboss on app 3 & 4
+
+
+echo "# Gittestgg" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Gauravg72116/Gittestgg.git
+git push -u origin main
+
+
+…or push an existing repository from the command line
+git remote add origin https://github.com/Gauravg72116/Gittestgg.git
+git branch -M main
+git push -u origin main
